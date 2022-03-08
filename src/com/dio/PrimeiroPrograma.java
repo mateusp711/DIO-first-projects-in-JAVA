4192e7df892929eef45336f0d882252dc64d66d6
Mateusp <mateus.p@gsuite.iff.edu.br>
2022-03-08 09:50:21 -0300

    second commit

--- src/com/dio/PrimeiroPrograma.java
+++ src/com/dio/PrimeiroPrograma.java
@@ -12,16 +12,26 @@ public class PrimeiroPrograma {
 		System.out.println(gato);
 		System.out.println(Livros);
 		
-		
-		
 		/*(int a = 2;
 		int b = 3;
 		System.out.println("Hello World! " + (a+b));*/
 	}
 }
 
-class Livros {
+class Livros {	
 	private String nome; 
 	private String npag;
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getNpag() {
+		return npag;
+	}
+	public void setNpag(String npag) {
+		this.npag = npag;
+	}
 	
 }
--- src/com/dio/moddel/Gato.java
+++ src/com/dio/moddel/Gato.java
@@ -2,42 +2,42 @@ package com.dio.moddel;
 
 public class Gato {
 	
-		private string nome; 
-		private string cor;
-		private integer idade;
+		private String nome; 
+		private String cor;
+		private Integer idade;
 		
 		
 		public Gato() {
 		}
 
-		public Gato(string nome, string cor, integer idade) {
+		public Gato(String nome, String cor, Integer idade) {
 			this.nome = nome;
 			this.cor = cor;
 			this.idade = idade;
 			
 		}
 
-		public string getNome() {
+		public String getNome() {
 			return nome;
 		}
 
-		public void setNome(string nome) {
+		public void setNome(String nome) {
 			this.nome = nome;
 		}
 
-		public string getCor() {
+		public String getCor() {
 			return cor;
 		}
 
-		public void setCor(string cor) {
+		public void setCor(String cor) {
 			this.cor = cor;
 		}
 
-		public integer getIdade() {
+		public Integer getIdade() {
 			return idade;
 		}
 
-		public void setIdade(integer idade) {
+		public void setIdade(Integer idade) {
 			this.idade = idade;
 		} 
 		
